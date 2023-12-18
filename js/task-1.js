fdd78242e9d23be16828f19e954ce57e60edf61d
PavlKud <144054118+PavlKud@users.noreply.github.com>
2023-12-18 20:26:37 +0100

    123

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
-const pricePerItem = 3500;
-const orderedQuantity = 4;
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits){
+        return "Insufficient funds!";
+    } else{
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    }
+    
+}
 
-// Change code below this line
 
-const totalPrice = pricePerItem * orderedQuantity;
-
-alert = totalPrice
-
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
