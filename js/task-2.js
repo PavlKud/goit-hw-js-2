edd574c452bf4d7a6f17ae69f248d09e7fa2a8af
PavlKud <144054118+PavlKud@users.noreply.github.com>
2023-12-18 21:37:17 +0100

    finish task 2,3,4

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,12 @@
 function formatMessage(message, maxLength){
-if (message.Length <= maxLength) {
-    return message;
-}else{  
-    return message.slice(0, maxLength) + "...";
-}
-}
-
+    if (message.length <= maxLength) {
+        
+        return message;
+      } else {
+        
+        return message.slice(0, maxLength) + "...";
+      }
+    }
 
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
